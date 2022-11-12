a9b2637b86f70dec889a45ac0137fa09685bd6eb
Marius THORRE <thorre.m@yahoo.com>
2022-11-12 13:49:45 +0100

    First commit

---
+++ src/main/java/com/firefighterstarter/FireFighterApp.java
@@ -0,0 +1,23 @@
+package com.firefighterstarter;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class FireFighterApp extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(FireFighterApp.class.getResource("fire-fighter.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Fire Fighter App");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/firefighterstarter/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.firefighterstarter.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("");
+    }
+}
