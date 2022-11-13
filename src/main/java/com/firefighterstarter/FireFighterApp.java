9b7206d9158d29a34e95ae987b888e269c7769dd
Marius THORRE <thorre.m@yahoo.com>
2022-11-13 12:41:27 +0100

    FF, re, cloud and way are painted

--- src/main/java/com/firefighterstarter/FireFighterApp.java
+++ src/main/java/com/firefighterstarter/FireFighterApp.java
@@ -3,6 +3,7 @@ package com.firefighterstarter;
 import com.firefighterstarter.view.grid.Grid;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,9 +13,15 @@ public class FireFighterApp extends Application {
     public void start(Stage stage) {
 
         Grid gridPaint = new Grid(1000, 1000, 1000, 1000);
+        gridPaint.maxHeight(1000);
+        gridPaint.maxWidth(1000);
         gridPaint.setStyle("-fx-background-color: white; -fx-grid-lines-visible: true");
+        Pane pane = new Pane();
+        pane.getChildren().add(gridPaint);
 
-        Scene scene = new Scene(gridPaint, 1500, 1000);
+        Scene scene = new Scene(pane, 1500, 1000);
+
+
 
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/firefighterstarter/view/grid/Grid.java
+++ src/main/java/com/firefighterstarter/view/grid/Grid.java
@@ -28,8 +28,9 @@ public class Grid extends GridPane {
         drawGrid();
         createSquare();
         paintWay();
+        paintCloud();
         paintFire();
-        //paintFF();
+        paintFF();
 
     }
 
@@ -55,14 +56,24 @@ public class Grid extends GridPane {
 
 
     public void paintFF() {
-        int randomSquare = (int) (Math.random() * (400-1));
-        this.getChildren().get(randomSquare).setStyle("-fx-background-color: grey;");
+        for(int i = 0; i < 900; i++) {
+            int randomNumber = (int) (Math.random() * 10);
+            if(i != 0) {
+                if(randomNumber % 5 == 0) {
+                    if(this.getChildren().get(i).getStyle().equals("-fx-background-color: #a5632a;"))
+                        this.getChildren().get(i).setStyle("-fx-background-color: black;");
+                }
+            }
+
+        }
     }
 
     public void paintFire() {
         for(int i = 0; i < 200; i++) {
             int randomSquare = (int) (Math.random() * (900-1));
-            if(this.getChildren().get(randomSquare).getStyle().equals("-fx-background-color: #a5632a;"))
+            // if square is cloud or way, don't paint a fire
+            if(this.getChildren().get(randomSquare).getStyle().equals("-fx-background-color: #a5632a;") ||
+                this.getChildren().get(randomSquare).getStyle().equals("-fx-background-color: #706969;"))
                 continue;
             this.getChildren().get(randomSquare).setStyle("-fx-background-color: red;");
         }
@@ -75,6 +86,15 @@ public class Grid extends GridPane {
             this.getChildren().get(i).setStyle("-fx-background-color: #a5632a;");
         for(int i = 315; i < 900; i += 30)
             this.getChildren().get(i).setStyle("-fx-background-color: #a5632a;");
+    }
+
+    public void paintCloud() {
+        for(int i = 0; i < 50; i++) {
+            int randomSquare = (int) (Math.random() * (900-1));
+            if(this.getChildren().get(randomSquare).getStyle().equals("-fx-background-color: #a5632a;"))
+                continue;
+            this.getChildren().get(randomSquare).setStyle("-fx-background-color: #706969;");
+        }
     }
 
 
